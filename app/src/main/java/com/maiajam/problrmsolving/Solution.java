60a64c07f0ebfc16a8160150f4f448931f5d4be1
mai waleed <maiajam88@hotmail.com>
2020-02-25 12:25:54 +0200

    add

--- app/src/main/java/com/maiajam/problrmsolving/Solution.java
+++ app/src/main/java/com/maiajam/problrmsolving/Solution.java
@@ -6,17 +6,20 @@ import android.widget.Toast;
 
 import java.util.Scanner;
 
+import static java.lang.String.valueOf;
+
 public class Solution {
 
+
+    private static String[] part;
+    String orignalString = "";
+    StringBuilder reversedString  ;
+
     public static String getSmallestAndLargest(String s, int k) {
-
-
         int sizeOfSubStringList = s.length()-(k-1);
         String[] listOfSubString = new String[sizeOfSubStringList];
-
         String smallest = "";
         String largest = "";
-
         for(int i = 0;i< s.length()-(k-1);i++)
         {
             listOfSubString[i]= s.substring(i,i+(k));
@@ -38,8 +41,6 @@ public class Solution {
             else
             largest = listOfSubString[x];
         }
-
-
         return smallest + "\n" + largest;
     }
 
@@ -49,7 +50,54 @@ public class Solution {
         String s = scan.next();
         int k = scan.nextInt();
         scan.close();
+    }
 
+    public static String reversString(String inputString)
+    {
+        StringBuilder reversedString = new StringBuilder();
+        reversedString.append(inputString);
+
+        reversedString = reversedString.reverse();
+
+        return reversedString.toString();
+    }
+
+    public static boolean checkPalindrome(String input)
+    {
+        if(input.compareTo(reversString(input))== 0)
+            return true;
+            return false ;
+    }
+
+    public static boolean splitString(String input)
+    {
+         part = input.split(".");
+        if(part.length != 4)
+            return false;
+        else
+            return true;
+    }
+
+    public boolean checkInteger(String input)
+    {
+
+        if(splitString(input))
+        {
+            for(int i= 0;i<part.length;i++)
+            {
+                String part1 = part[i];
+                try {
+                    int x = Integer.valueOf(part1);
+                }catch (NumberFormatException nfe)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }else
+        {
+            return false ;
+        }
 
     }
 }
